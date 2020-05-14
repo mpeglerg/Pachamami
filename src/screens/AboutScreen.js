463cd777f44b6bc9b962455723f46bbf04ca819c
mpeglerg <mpeglerg@lion.lmu.edu>
2020-05-14 18:47:35 -0400

    basic about screen edits

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import HomePageCarousel1 from "/Users/mayapeglergordon/Pachamami/Pachamami/src/images/HomePageCarousel1.jpeg";
 
 const AboutScreen = ({ navigation }) => {
   return (
-    <View
-      title
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>About Screen</Text>
+    <View title style={{ flex: 1, alignItems: "center" }}>
+      <Image
+        source={HomePageCarousel1}
+        style={{ width: 380, height: 250 }}></Image>
+      <Text>Pachamami's Mission</Text>
+      <Text>About the Founder</Text>
     </View>
   );
 };
