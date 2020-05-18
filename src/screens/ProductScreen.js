1630f3474f1ed3c3182d705fee5bae80e4b1c20f
mpeglerg <mpeglerg@lion.lmu.edu>
2020-05-17 22:37:15 -0400

    fix error

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -34,14 +34,7 @@ const ProductScreen = ({ navigation }) => {
           <Button title="Product Description"></Button>
         </TouchableHighlight>
       </View>
-
-      {/* <Button title="Show panel" onPress={() => this._panel.show()} />
-      <SlidingUpPanel ref={(c) => (this._panel = c)}>
-        <View style={styles.container}>
-          <Text>Here is the content inside panel</Text>
-          <Button title="Hide" onPress={() => this._panel.hide()} />
-        </View>
-      </SlidingUpPanel> */}
+    </ScrollView>
   );
 };
 
